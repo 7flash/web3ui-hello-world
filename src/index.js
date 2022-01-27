d0a3588458287ce188d6051a6082eafc1a57069f
Igor Berlenko <sevenflash@yandex.ru>
2022-01-28 02:22:12 +0800

    hello world

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { CryptoCards, Button } from 'web3uikit';
+
+const App = () => (
+    <>
+        <h1>
+            Hello, world!
+        </h1>
+    </>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
